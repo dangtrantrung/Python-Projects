2ac12fe429b22d79eac5fb4e180dc025dff6d076
dangtrantrung <trung280584@gmail.com>
2024-02-14 14:29:09 +0700

    Voice command on Google and Youtube search

--- friday.py
+++ friday.py
@@ -1,5 +1,9 @@
 import pyttsx3
 import datetime
+# speech to text
+import speech_recognition as sr
+import webbrowser as wb
+
 
 friday=pyttsx3.init()
 voice=friday.getProperty('voices')
@@ -26,5 +30,34 @@ def welcome():
         speak("Good night sir")
     speak("I'm Friday AI Assistant. How can i help you?")
 
+def command():
+    c=sr.Recognizer()
+    with sr.Microphone() as source:
+        c.pause_threshold=2
+        audio=c.listen(source)
+    try:
+        query=c.recognize_google(audio,language='en')
+        print("Tony Tèo: "+query)
+    except sr.UnknownValueError:
+        print ("Please repeat or typing the command")
+        query=str(input('Your order is: '))
+    return query
+
+
 if __name__=="__main__":
     welcome()
+    while True:
+        query=command().lower()
+        if 'google' in query:
+            speak('What should I search boss?')
+            search=command().lower()
+
+            url=f'https://google.com/search?q={search}'
+            wb.get().open(url)
+            speak(f'Here is your {search} on Google')
+        elif 'youtube' in query:
+            speak('What should I search boss?')
+            search=command().lower()
+            url=f'https://youtube.com/search?q={search}'
+            wb.get().open(url)
+            speak(f'Here is your {search} on youtube')
